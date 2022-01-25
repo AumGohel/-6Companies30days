6662141cda607179269dc4ee7c3df0fc8ab8b48d
Aum Gohel <60268340+AumGohel@users.noreply.github.com>
2022-01-25 18:05:34 +0530

    Create Q15_Koko Eating Bananas.cpp

---
+++ Intuit/Q15_Koko Eating Bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = -1;
+        for( auto k : piles){
+            if(k>right){
+                right = k;
+            }
+        }
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canEatInTime(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+    bool canEatInTime(vector<int>& piles, int k, int h){
+        int hours = 0;
+        for(int pile : piles){
+            int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++;
+        }
+        return hours <= h;
+    }
+};
