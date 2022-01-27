85bd13a7f29e33d4c3db44ea295b3b679faf4495
Aum Gohel <60268340+AumGohel@users.noreply.github.com>
2022-01-27 12:50:04 +0530

    Create Q08_Maximum Height Tree.cpp

---
+++ Walmart/Q08_Maximum Height Tree.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        // code here
+        int ans =1;
+        int i=1;
+        while(ans<=N){
+            ans+= i+1;
+            i++;
+        }
+        return i-1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
