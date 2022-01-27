9f11a8777864982c63a51d73487897b3e77c48c2
Aum Gohel <60268340+AumGohel@users.noreply.github.com>
2022-01-27 13:05:22 +0530

    Create Q13_Find the Kth Largest Integer in the Array (Cows and Farm Question).cpp

---
+++ Walmart/Q13_Find the Kth Largest Integer in the Array (Cows and Farm Question).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    static bool compare(string &a,string &b){
+        if(a.size()==b.size()){
+            return a<b;
+        }
+        return a.size()<b.size();
+    }
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(),nums.end(),compare);
+        return nums[nums.size()-k];
+    }
+};
